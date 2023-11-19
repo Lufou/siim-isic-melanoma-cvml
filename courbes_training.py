ee5e4307d5f1ef85ceb037432e74af0dc2b595c7
SalmaBahar <91837917+SalmaBahar@users.noreply.github.com>
2023-11-19 13:57:37 +0100

    Update courbes_training.py

--- courbes_training.py
+++ courbes_training.py
@@ -7,15 +7,13 @@ df = pd.read_csv("effnet_28K/training_results.csv")
 # Create a TensorBoard SummaryWriter
 writer = SummaryWriter()
 
-# Log the training loss and accuracy
+# Log the training and validation loss
 for epoch, row in df.iterrows():
-    writer.add_scalar("Train/Loss", row["train_loss"], epoch)
-    writer.add_scalar("Train/Accuracy", row["train_accuracy"], epoch)
+    writer.add_scalars("Loss: Training(red) and Validation(blue)", {"Train": row["train_loss"], "Validation": row["val_loss"]}, epoch)
 
-# Log the validation loss and accuracy
+# Log the training and validation accuracy
 for epoch, row in df.iterrows():
-    writer.add_scalar("Validation/Loss", row["val_loss"], epoch)
-    writer.add_scalar("Validation/Accuracy", row["val_accuracy"], epoch)
+    writer.add_scalars("Accuracy: Training(orange) and Validation(green)", {"Train": row["train_accuracy"], "Validation": row["val_accuracy"]}, epoch)
 
 # Close the SummaryWriter
 writer.close()
