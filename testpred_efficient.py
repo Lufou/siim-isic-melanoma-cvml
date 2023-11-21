4c87a0431543308274f45b14733d50b5f74aa615
c-lee17 <92338357+c-lee17@users.noreply.github.com>
2023-11-21 19:24:17 +0100

    Supp comm testpred_efficient.py

--- testpred_efficient.py
+++ testpred_efficient.py
@@ -6,26 +6,21 @@ from torchvision import transforms
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import timm  # Importez la bibliothèque timm pour EfficientNet
+import timm
 import torch.nn.functional as F
 
-# Load the pretrained model from the file (make sure to adjust the file path)
 checkpoint = torch.load("hair_efficientnet_b0_20ep_64batch.pth")
 
-# Set up transformations similar to those used for training
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# Directory containing test images
 test_directory = "test-resized"
 
-# List of test image names
 test_image_names = [f.replace(".jpg", "") for f in os.listdir(test_directory) if f.endswith(".jpg")]
 
-# Create a custom dataset for test data
 class TestDataset(Dataset):
     def __init__(self, image_names, transform=None):
         self.image_names = image_names
@@ -41,35 +36,29 @@ class TestDataset(Dataset):
             image = self.transform(image)
         return image
 
-# Create the dataset for test data
 test_dataset = TestDataset(test_image_names, transform=transform)
 
-# Load your new test data using DataLoaders
-batch_size = 64  # Choose the appropriate batch size
+batch_size = 64 
 test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-# Load the EfficientNet model using timm
-efficientnet_b0 = timm.create_model('efficientnet_b0', pretrained=False, num_classes=2)  # Set pretrained=False
+# Charger le modèle EfficientNet
+efficientnet_b0 = timm.create_model('efficientnet_b0', pretrained=False, num_classes=2)
 efficientnet_b0.load_state_dict(checkpoint['model'])
 efficientnet_b0 = efficientnet_b0.eval()
 
-# Create a list to store predictions
 probabilities = []
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Use a loop to make predictions on test data
+# Prédictions sur les données
 with torch.no_grad():
     for inputs in test_loader:
         inputs = inputs.to(device)
         outputs = efficientnet_b0(inputs)
         probabilities.extend(torch.softmax(outputs, dim=1)[:, 1].cpu().numpy())
 
-# Now, the list "probabilities" contains the predicted probabilities for each test image
 
-# Create a pandas DataFrame with image names and predictions
 results = pd.DataFrame({'image_name': test_image_names, 'probability': probabilities})
 
-# Save the DataFrame to a CSV file
 results.to_csv('hair_efficientnet_b0_20ep_64batch.csv', index=False)
 
