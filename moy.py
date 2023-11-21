d28ceea593c1138c0da1529fdeadd5ed164875cb
SalmaBahar <91837917+SalmaBahar@users.noreply.github.com>
2023-11-21 19:01:32 +0100

    Update moy.py

--- moy.py
+++ moy.py
@@ -1,24 +1,21 @@
 import pandas as pd
+file_path1 = "28K_efficientnet_b3_15ep_64batch.csv"
+file_path2 = "28K_resnet50_25e_batch64.csv"
+file_path3 = "28K_seresnext50d_15ep_64batch.csv"
+file_path4 = "44K_efficientnet_b3_15ep_64batch.csv"
+file_path5 = "44K_seresnext50_15ep_64batch.csv"
+#file_path6 = "44K_resnet50_30ep_batch64.csv"
 
-# Charger les fichiers CSV
-file_path1 = "hair_16000nomel_3500mel_resnet18_20ep_64batch.csv"
-file_path2 = "hair_16000nomel_3500mel_resnet34_15ep_64batch.csv"
-file_path3 = "hair_efficientnet_b0_20ep_64batch.csv"
-file_path4 = "hair_seresnext26d_15ep_64batch.csv"
 df1 = pd.read_csv(file_path1)
 df2 = pd.read_csv(file_path2)
 df3 = pd.read_csv(file_path3)
 df4 = pd.read_csv(file_path4)
+df5 = pd.read_csv(file_path5)
+#df6 = pd.read_csv(file_path6)
 
-# Assurez-vous que les deux dataframes ont la même structure
-if not all(df1.columns == df2.columns) or not all(df2.columns == df3.columns) or not all(df3.columns == df4.columns):
-    raise ValueError("Les colonnes des deux fichiers CSV ne correspondent pas.")
+# moyenne de la colonne "target"
+df1['target'] = (2*df1['target'] + df2['target'] + 2*df3['target']+ df4['target']+ df5['target'])/7
 
-# Calculer la moyenne de la colonne "target"
-df1['target'] = (df1['target'] + df2['target'] +df3['target']+ df4['target'])/ 4
-
-# Sauvegarder le résultat dans un nouveau fichier CSV
-output_file_path = "testmoy3.csv"
+output_file_path = "Moyenne.csv"
 df1.to_csv(output_file_path, index=False)
-
 print(f"La moyenne des colonnes 'target' a été sauvegardée dans {output_file_path}")
